a5cc683c6c46d36d04c43fde075aba9cf4e9c055
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-09-15 20:47:43 +0300

    feat: handle wrong input

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import { SecretKey } from './secret-key.js';
 import { Moves } from './moves.js';
 import { Win } from './win.js';
 
-const moves = process.argv;
+const moves = process.argv.slice(2);
+console.log('Start');
+
 const key = SecretKey.getSecretKey();
 const PCMoveIndex = Moves.getPCMove(moves);
-console.log('Start');
-console.log(`${PCMoveIndex} and ${key}`);
-const UserMove = readlineSync.question('Your move:');
-console.log('Your move is ' + UserMove);
+const hmac = SecretKey.getHMAC(key, moves[PCMoveIndex]);
+console.log(hmac);
+const userMove = readlineSync.question('Your move:');
+if (!parseInt(userMove)) {
+  console.log(`Wrong input. Should be only numbers from 1 to ${moves.length}`);
+  // TODO block game until correct input
+}
+console.log('Your move is ' + userMove);
 const sideMovesAmount = Math.floor(moves.length / 2);
 const isPCWin = Math.sign(
-  ((PCMoveIndex - UserMove + sideMovesAmount + moves.length) % moves.length) -
+  ((PCMoveIndex - userMove + sideMovesAmount + moves.length) % moves.length) -
     sideMovesAmount
 );
 Win.declareWinner(isPCWin);
+console.log('PC move is ' + moves[PCMoveIndex]);
 console.log('Finish');
--- secret-key.js
+++ secret-key.js
@@ -1,8 +1,11 @@
-import { randomBytes } from 'crypto';
+import { createHmac, randomBytes } from 'crypto';
 
 export class SecretKey {
   static getSecretKey() {
     const secretKey = randomBytes(32).toString('hex');
     return secretKey;
+  }
+  static getHMAC(secretKey, message) {
+    return createHmac('sha256', secretKey).update(message).digest('hex');
   }
 }
