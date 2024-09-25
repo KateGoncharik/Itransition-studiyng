555dd337e5b93a264f4dbd20d812c2c31c7fa782
Kate <kate.goncharik.artist@gmail.com>
2024-09-25 13:46:38 +0300

    fix: block login when user is not in db, but in auth

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
-import { Component } from '../component.js';
-import { auth, db } from '../firebase-config.js';
-import { createMainPage } from './components/main/main-page.js';
-import { getUserByEmail } from './components/main/get-user-by-email.js';
-import { renderUserTable } from './components/main/users-table.js';
-import { nav } from './components/nav/nav.js';
-import { updateNavButtons } from './components/nav/update-nav-buttons.js';
+import { Component } from "../component.js";
+import { auth, db } from "../firebase-config.js";
+import { createMainPage } from "./components/main/main-page.js";
+import { getUserByEmail } from "./components/main/get-user-by-email.js";
+import { renderUserTable } from "./components/main/users-table.js";
+import { nav } from "./components/nav/nav.js";
+import { updateNavButtons } from "./components/nav/update-nav-buttons.js";
 import {
   collection,
   onSnapshot,
+  doc,
+  getDoc,
   getDocs,
-} from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
-import { renderLoginPage } from './components/auth/login-page.js';
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { renderLoginPage } from "./components/auth/login-page.js";
+import { formatEmail } from "./components/auth/format-email.js";
 
 export const app = new Component(
   {},
   new Component(
-    { className: 'wrapper container-fluid' },
+    { className: "wrapper container-fluid" },
     nav,
     new Component({
-      className: 'page-content container-fluid',
+      className: "page-content container-fluid",
     })
   )
 );
@@ -27,15 +30,29 @@ let unsubscribeFromUsers = null;
 
 auth.onAuthStateChanged(async (user) => {
   if (user) {
+    // auth.signOut();
+    const userDocRef = doc(db, "users", formatEmail(user.email));
+
+    const ssss = await getDoc(userDocRef);
+    console.log(userDocRef);
+
+    if (!ssss.exists()) {
+      auth.signOut();
+      const errorField = document.querySelector(".error-field");
+      errorField.classList.remove("opacity-0");
+      errorField.classList.add("opacity-100");
+      errorField.innerHTML = `No user with email ${user.email} found. Register first`;
+      return;
+    }
+
     const userInDB = await getUserByEmail(user.email);
-    const isUserBlocked = userInDB.status === 'blocked';
+    const isUserBlocked = userInDB.status === "blocked";
     if (isUserBlocked) {
       updateNavButtons(false);
       renderLoginPage();
       return;
     }
-    const usersCollection = collection(db, 'users');
-
+    const usersCollection = collection(db, "users");
     unsubscribeFromUsers = onSnapshot(usersCollection, (snapshot) => {
       const users = [];
       snapshot.forEach((doc) => {
@@ -44,11 +61,9 @@ auth.onAuthStateChanged(async (user) => {
       createMainPage();
       renderUserTable(users);
     });
-
     getDocs(usersCollection).then((snapshot) => {
       renderUserTable(snapshot.docs);
     });
-
     updateNavButtons(true);
   } else {
     if (unsubscribeFromUsers) {
--- src/components/auth/login-page.js
+++ src/components/auth/login-page.js
@@ -1,81 +1,82 @@
-import { Component } from '../../../component.js';
+import { Component } from "../../../component.js";
 
-import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 import {
   doc,
   updateDoc,
+  getDoc,
   serverTimestamp,
-} from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
-import { getUserByEmail } from '../main/get-user-by-email.js';
-import { auth, db } from '../../../firebase-config.js';
-import { updateTitle } from './update-title.js';
-import { renderRegistrationPage } from './registration-page.js';
-import { formatEmail } from './format-email.js';
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getUserByEmail } from "../main/get-user-by-email.js";
+import { auth, db } from "../../../firebase-config.js";
+import { updateTitle } from "./update-title.js";
+import { renderRegistrationPage } from "./registration-page.js";
+import { formatEmail } from "./format-email.js";
 
 export const loginForm = new Component({
-  tag: 'form',
-  className: 'w-50 m-auto mt-3',
+  tag: "form",
+  className: "w-50 m-auto mt-3",
 });
-loginForm.setAttribute('id', 'login-form ');
+loginForm.setAttribute("id", "login-form ");
 
 const emailInput = new Component({
-  tag: 'input',
-  className: 'login-email form-control',
-  text: '',
+  tag: "input",
+  className: "login-email form-control",
+  text: "",
 });
-emailInput.setAttribute('type', 'email');
-emailInput.setAttribute('id', 'login-email');
-emailInput.setAttribute('required', true);
+emailInput.setAttribute("type", "email");
+emailInput.setAttribute("id", "login-email");
+emailInput.setAttribute("required", true);
 
 const emailLabel = new Component({
-  tag: 'label',
-  className: '',
-  text: 'Email address',
+  tag: "label",
+  className: "",
+  text: "Email address",
 });
-emailLabel.setAttribute('for', 'login-email');
+emailLabel.setAttribute("for", "login-email");
 
 const emailField = new Component(
-  { tag: 'div', className: 'form-group' },
+  { tag: "div", className: "form-group" },
   emailLabel,
   emailInput
 );
 
 const passwordInput = new Component({
-  tag: 'input',
-  className: 'login-password form-control',
-  text: '',
+  tag: "input",
+  className: "login-password form-control",
+  text: "",
 });
-passwordInput.setAttribute('type', 'password');
-passwordInput.setAttribute('id', 'login-password');
-passwordInput.setAttribute('required', true);
+passwordInput.setAttribute("type", "password");
+passwordInput.setAttribute("id", "login-password");
+passwordInput.setAttribute("required", true);
 
 const passwordLabel = new Component({
-  tag: 'label',
-  className: '',
-  text: 'Your password',
+  tag: "label",
+  className: "",
+  text: "Your password",
 });
-passwordLabel.setAttribute('for', 'login-password');
+passwordLabel.setAttribute("for", "login-password");
 
 const passwordField = new Component(
-  { tag: 'div', className: 'form-group' },
+  { tag: "div", className: "form-group" },
   passwordLabel,
   passwordInput
 );
 
 const submitButton = new Component({
-  tag: 'button',
-  className: 'btn btn-primary mt-2',
-  text: 'Submit',
+  tag: "button",
+  className: "btn btn-primary mt-2",
+  text: "Submit",
 });
 
 const errorField = new Component({
-  className: 'error-field opacity-0 text-danger',
+  className: "error-field opacity-0 text-danger",
 });
 const registrationButton = new Component({
-  className: 'btn text-info border-none',
-  text: 'account',
+  className: "btn text-info border-none",
+  text: "account",
 });
-registrationButton.addListener('click', (e) => {
+registrationButton.addListener("click", (e) => {
   e.preventDefault();
   renderRegistrationPage();
 });
@@ -84,51 +85,59 @@ loginForm.appendChildren([
   passwordField,
   errorField,
   new Component(
-    { className: '', text: 'New here? Create your ' },
+    { className: "", text: "New here? Create your " },
     registrationButton
   ),
   submitButton,
 ]);
 
-loginForm.addListener('submit', async (e) => {
+loginForm.addListener("submit", async (e) => {
   e.preventDefault();
-  const loginEmail = document.querySelector('.login-email');
-  const loginPassword = document.querySelector('.login-password');
+  const loginEmail = document.querySelector(".login-email");
+  const loginPassword = document.querySelector(".login-password");
 
   const email = loginEmail.value.trim().toLowerCase();
   const password = loginPassword.value.trim();
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
+    const userDocRef = doc(db, "users", formatEmail(email));
+
+    const ssss = await getDoc(userDocRef);
+
+    if (!ssss.exists()) {
+      return;
+    }
+
     updateTitle();
 
-    const userDoc = doc(db, 'users', formatEmail(email));
+    const userDoc = doc(db, "users", formatEmail(email));
     await updateDoc(userDoc, {
       lastLogin: serverTimestamp(),
     }).catch((error) => {
-      console.error('Error while updating last login: ', error);
+      console.error("Error while updating last login: ", error);
     });
     const user = await getUserByEmail(email);
-    if (user.status === 'blocked') {
+    if (user.status === "blocked") {
       auth.signOut();
     }
   } catch (error) {
-    const errorField = document.querySelector('.error-field');
-    errorField.classList.remove('opacity-0');
-    errorField.classList.add('opacity-100');
+    const errorField = document.querySelector(".error-field");
+    errorField.classList.remove("opacity-0");
+    errorField.classList.add("opacity-100");
     errorField.innerHTML = error.message;
-    console.error('Error during login:', error.message);
+    console.error("Error during login:", error.message);
   }
 });
 
 export function renderLoginPage() {
-  const container = document.querySelector('.page-content');
+  const container = document.querySelector(".page-content");
 
-  container.innerHTML = '';
+  container.innerHTML = "";
   container.appendChild(
     new Component(
-      { className: 'login-page' },
-      new Component({ tag: 'h1', text: 'Login', className: 'text-center m-3' }),
+      { className: "login-page" },
+      new Component({ tag: "h1", text: "Login", className: "text-center m-3" }),
       loginForm
     ).getNode()
   );
