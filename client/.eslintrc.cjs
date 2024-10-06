a5661ffd98f8e6678f13e3b9a04a96154fed57d0
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-10-06 19:53:51 +0300

    config: add favicon, prettier

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,57 +5,56 @@
 module.exports = {
   env: { browser: true, es2023: true },
   extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:perfectionist/recommended-natural',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
   ],
   overrides: [
     {
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
-      files: ['./**/*.+(cjs|js|mjs)'],
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: ["./**/*.+(cjs|js|mjs)"],
     },
     {
       env: { node: true },
-      files: ['./**/*.cjs'],
+      files: ["./**/*.cjs"],
     },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.node.json'],
+    project: ["tsconfig.json", "tsconfig.node.json"],
     tsConfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'react-refresh'],
+  plugins: ["@typescript-eslint", "react-refresh"],
   root: true,
   rules: {
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
       {
         allowExpressions: true,
       },
     ],
-    '@typescript-eslint/no-explicit-any': 'error',
-    curly: ['error', 'all'],
-    'react-refresh/only-export-components': [
-      'warn',
+    "@typescript-eslint/no-explicit-any": "error",
+    curly: ["error", "all"],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
-    'import/resolver': {
+    "import/resolver": {
       typescript: true,
     },
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 };
--- client/get-url.ts
+++ client/get-url.ts
@@ -1,4 +1,4 @@
-import { SERVER_HOST } from "./params.js";
+import { SERVER_HOST } from "./params.ts";
 export function getUrl(path: string) {
   return `${SERVER_HOST}/${path}`;
 }
