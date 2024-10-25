6001b0531be50d4a7010efb49b120b266177f5e6
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-10-25 12:54:46 +0300

    feat: add animation on 404

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -1,11 +1,36 @@
+import { Button, Stack, Typography } from "@mui/material";
 import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import Lottie from "react-lottie";
+import animationData from "../lotties/Animation - 1729849078176.json";
 
 export default function NotFoundPage(): ReactNode {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <>
-      Oh, hi! Sorry, but here is nothing to do.
-      {`404 - Page ${window.location.pathname} was not found`}
-      <a href="/">Back to main</a>
-    </>
+    <Stack sx={{ margin: "2% auto", gap: 1, alignItems: "center" }}>
+      <Typography component="h1" variant="h2">
+        Oh, hi!
+      </Typography>
+      <Typography variant="h4" color="info">
+        404
+      </Typography>
+
+      <Lottie options={defaultOptions} height={300} width={300} />
+      <Typography> Sorry, but here is nothing to do</Typography>
+
+      <Typography>
+        {`Page "${window.location.pathname}" does not exist`}
+      </Typography>
+      <Link to="/">
+        <Button variant="contained">Back to main</Button>
+      </Link>
+    </Stack>
   );
 }
