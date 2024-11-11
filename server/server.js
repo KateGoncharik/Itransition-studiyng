5e66fc7a23a52d83f5b50095f77f0bd890b7923c
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-11-11 12:59:22 +0300

    fix: try to figure out why error handler does not work

--- server/server.js
+++ server/server.js
@@ -70,19 +70,30 @@ function handleDisconnect() {
     }
   });
 
-  db.on("error", (err) => {
-    console.error("DB error in error handler:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST" || err.code === "ECONNRESET") {
-      console.log("Connection lost, reconnecting...");
-      handleDisconnect();
-    } else {
-      console.error(ERRORS.dbConnection, err);
-      throw err;
-    }
-  });
+  // db.on("error", (err) => {
+  //   console.error("DB error in error handler:", err);
+  //   if (err.code === "PROTOCOL_CONNECTION_LOST" || err.code === "ECONNRESET") {
+  //     console.log("Connection lost, reconnecting...");
+  //     handleDisconnect();
+  //   } else {
+  //     console.error(ERRORS.dbConnection, err);
+  //     throw err;
+  //   }
+  // });
 }
 
 handleDisconnect();
+
+db.on("error", (err) => {
+  console.error("DB error in error handler:", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST" || err.code === "ECONNRESET") {
+    console.log("Connection lost, reconnecting...");
+    handleDisconnect();
+  } else {
+    console.error(ERRORS.dbConnection, err);
+    throw err;
+  }
+});
 
 app.get("/users", (req, res) => {
   getUserByToken(req, res, async () => {
